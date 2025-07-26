dfbc0ca8eaa6a2e9445284c9722a0da17b972698
Ferguson Iyara <fergusoniyara@gmail.com>
2025-07-26 15:45:50 +0100

    chore: Update imports

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,12 +1,14 @@
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseService } from './database/services/database.service';
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UserModule,
   ],
   providers: [DatabaseService],
 })
--- apps/backend/src/user/user.module.ts
+++ apps/backend/src/user/user.module.ts
@@ -1,11 +1,12 @@
 import { CqrsModule } from '@nestjs/cqrs';
 import { Module } from '@nestjs/common';
 import { UserFactory } from './domain/factories/user.factory';
+import { UserRepository } from './domain/repositories/user.repository';
 import { UserService } from './domain/services/user.service';
 
 @Module({
   imports: [CqrsModule],
-  providers: [UserFactory, UserService],
-  exports: [UserFactory, UserService],
+  providers: [UserFactory, UserService, UserRepository],
+  exports: [UserFactory, UserService, UserRepository],
 })
 export class UserModule {}
